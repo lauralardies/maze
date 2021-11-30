1e520177b971a7909ed366f8427205ff56aa951b
lauralardies <lrodrlar@myuax.com>
2021-11-30 10:25:11 +0100

    no funciona pero luego lo arreglo

--- maze.py
+++ maze.py
@@ -32,9 +32,32 @@ def createMaze(coordinates, start, finish):
     return maze
 
 maze = createMaze(coordinates, start, finish) 
+position = start
+movements = []
 
 print(maze[0])
 print(maze[1])
 print(maze[2])
 print(maze[3])
 print(maze[4])
+
+while True: 
+    while position in range(5):
+        if " " ==  position[0] + 1:
+            movements.append("Down")
+            position = [position[0] + 1, position[1]]
+        elif " " == position[0] - 1: 
+            movements.append("Up")
+            position = [position[0] - 1, position[1]]
+        elif " " == position[1] + 1: 
+            movements.append("Right")
+            position = [position[0], position[1] + 1]
+        elif " " == position[1] - 1: 
+            movements.append("Left")
+            position = [position[0], position[1] - 1]
+        elif position == finish:
+
+            break
+    break
+
+print("The maze has been finished with this list of movements: \n" + str(movements))
