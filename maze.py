c877e536b85c1620c1ac449965c9f0c399a6ed76
lauralardies <lrodrlar@myuax.com>
2021-12-01 08:39:40 +0100

    Arreglado y código final

--- maze.py
+++ maze.py
@@ -14,6 +14,7 @@ coordinates = (
             )
 start = [0,0]
 finish = [4,4]
+position = start
 
 def createMaze(coordinates, start, finish):
     maze = []
@@ -31,43 +32,47 @@ def createMaze(coordinates, start, finish):
         maze.append(row)
     return maze
 
-maze = createMaze(coordinates, start, finish) 
-position = start
+def validPosition(x, y):
+    if (x < 0) | (y < 0):
+        return False
+    if (x > 4) | (y > 4):
+        return False
+    if maze[x][y] == " ":
+        return True
+    if maze[x][y] == "F":
+        return True
+    return False
 
-def validPosition(position):
-    for i in position:
-        if any(i < 0):
-            return False
-        if any(i == "X"):
-            return False
-        if any(i == " "):
-            return True
+def solveMaze(maze, position):
+    movements = []
+    while len(movements) >= 0:
+        if validPosition(position[0] + 1, position[1]):
+            maze[position[0]][position[1]] = "-"
+            position = [position[0] + 1, position[1]]
+            movements.append("Down")
+        if validPosition(position[0] - 1, position[1]):
+            maze[position[0]][position[1]] = "-"
+            position = [position[0] - 1, position[1]]
+            movements.append("Up")
+        if validPosition(position[0],position[1] + 1):
+            maze[position[0]][position[1]] = "-"
+            position = [position[0], position[1] + 1]
+            movements.append("Right")
+        if validPosition(position[0], position[1] - 1):
+            maze[position[0]][position[1]] = "-"
+            position = [position[0], position[1] - 1]
+            movements.append("Left")
+        if maze[position[0]][position[1]] == "F":
+            break
+    return movements
+
+maze = createMaze(coordinates, start, finish) 
+movements = solveMaze(maze, position)
 
 print(maze[0])
 print(maze[1])
 print(maze[2])
 print(maze[3])
 print(maze[4])
-
-def solveMaze(maze, position):
-    movements = []
-    while len(movements) >= 0:
-        if validPosition(position[0] + 1):
-            position = [position[0] + 1, position[1]]
-            movements.append("Down")
-        if validPosition(position[0] - 1):
-            position = [position[0] - 1, position[1]]
-            movements.append("Up")
-        if validPosition(position[1] + 1):
-            position = [position[0], position[1] + 1]
-            movements.append("Right")
-        if validPosition(position[1] - 1):
-            position = [position[0], position[1] - 1]
-            movements.append("Left")
-        if validPosition(position) == "F":
-            break
-    return movements
-
-movements = solveMaze(maze, position)
 
 print("The maze has been finished with this list of movements:\n" + str(movements))
